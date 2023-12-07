c15a6887848e22761244bdb01908ba55b7a450fb
Hamza Baig <mail.hamzabaig@gmail.com>
2023-12-07 14:30:38 +0500

    Create animals table in vet_clinic DB

--- schema.sql
+++ schema.sql
@@ -1,4 +1,14 @@
 /* Database schema to keep the structure of entire database. */
 
 createdb -U hamza vet_clinic
+
 psql -d vet_clinic
+
+CREATE TABLE animals (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR,
+    date_of_birth DATE,
+    escape_attempts INTEGER,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
+);
