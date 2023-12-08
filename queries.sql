bf54809bd87e5e655f438f3f8c28fbebd1d526ad
Hamza Baig <mail.hamzabaig@gmail.com>
2023-12-09 00:04:36 +0500

    Perform various analytics queries

--- queries.sql
+++ queries.sql
@@ -31,15 +31,43 @@ SELECT * FROM animals;
 ROLLBACK; 
 SELECT * FROM animals;
 
-BEGIN; DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+BEGIN; 
+DELETE FROM animals 
+WHERE date_of_birth > '2022-01-01';
 
 SAVEPOINT before_update;
 
-UPDATE animals SET weight_kg = weight_kg * -1;
+UPDATE animals 
+SET weight_kg = weight_kg * -1;
 
 ROLLBACK TO before_update;
 
-UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+UPDATE animals 
+SET weight_kg = weight_kg * -1 
+WHERE weight_kg < 0;
+
+SELECT COUNT(*) FROM animals;
+
+SELECT COUNT(*) FROM animals 
+WHERE escape_attempts = 0;
+
+SELECT AVG(weight_kg) FROM animals;
+
+SELECT neutered, COUNT(*) AS escape_count
+FROM animals
+WHERE escape_attempts > 0
+GROUP BY neutered;
+
+SELECT species, MIN(weight_kg), MAX(weight_kg)
+FROM animals
+GROUP BY species;
+
+SELECT species, AVG(escape_attempts)
+FROM animals
+WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31'
+GROUP BY species;
+
+
 
 
 
