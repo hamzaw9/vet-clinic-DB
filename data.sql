f42ea573185c5f1950bba6647e684ac78d28cb12
Hamza Baig <mail.hamzabaig@gmail.com>
2023-12-09 12:07:26 +0500

    Insert data into owners table

--- data.sql
+++ data.sql
@@ -7,7 +7,6 @@ VALUES
     ('Pikachu', '2021-01-07', 1, false, 15.04),
     ('Devimon', '2017-05-12', 5, true, 11);
 
-
 INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
 VALUES
   ('Charmander', '2020-02-08', 0, FALSE, -11),
@@ -17,3 +16,13 @@ VALUES
   ('Boarmon', '2005-06-07', 7, TRUE, 20.4),
   ('Blossom', '1998-10-13', 3, TRUE, 17),
   ('Ditto', '2022-05-14', 4, TRUE, 22);
+
+INSERT INTO owners (full_name, age) VALUES
+    ('Sam Smith', 34),
+    ('Jennifer Orwell', 19),
+    ('Bob', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+
+
